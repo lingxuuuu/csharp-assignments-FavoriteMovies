7097e78e9bada7bca8f6997d6fc30e10dc5f3724
Ling Xu <36904516+xulingggg@users.noreply.github.com>
2020-12-17 14:20:16 -0800

    commit on master

--- FavoriteMovies/Controllers/HomeController.cs
+++ FavoriteMovies/Controllers/HomeController.cs
@@ -41,6 +41,7 @@ public class HomeController : Controller
         .Include(movie => movie.PostedBy)
         // many to many relationship, need to have .Include() to access the Likes in Dashboard.cshtml
         .Include(movie => movie.Likes)
+        .OrderByDescending(movie => movie.Likes.Count)
         .ToList();
 
         return View();
@@ -50,9 +51,20 @@ public class HomeController : Controller
     [HttpGet("movies/new")] //this is the actual route in the address bar (action without asp points to this url)
     public IActionResult NewMoviePage() //the action name can be used with asp-action on cshtml page
     {
+
+        int? userId = HttpContext.Session.GetInt32("UserId"); //int or null
+
+        if (userId == null)
+        {
+            return RedirectToAction("LoginReg", "Users"); //action: go to LoginReg page, controller: UsersController//
+        }
+
+        ViewBag.User = _context
+            .Users
+            .Find(userId);
+
         return View();
     }
-
 
     [HttpPost("movies")] //this is the actual route in the address bar (action without asp points to this url)
     public IActionResult CreateMovie(Movie movieToCreate) //the action name can be used with asp-action on cshtml page
@@ -82,7 +94,7 @@ public class HomeController : Controller
     }
 
     //This line of code is responding for the Dashboard.cshtml MoviePage to show individual movie
-    [HttpPost("movies/{id}")]
+    [HttpGet("movies/{id}")] //post is submitting the form, get is retriving the info
     public IActionResult MoviePage(int id)
     {
         //retrieve the movie first
@@ -90,7 +102,16 @@ public class HomeController : Controller
             .Movies
             //one to many relationship: each movie I find, I want to return the movie that is PostedBy
             .Include(movie => movie.PostedBy)
+            //populate the likes first
+            .Include(movie => movie.Likes)
+                //and then populate the users
+                .ThenInclude(like => like.UserWhoLikes)
             .FirstOrDefault(movie => movie.MovieId == id); //take a movie, see if the the MovieId is the same with provided id
+
+        //we want to have a delete button on MoviePage, so we need to get the User in the cshtml as well
+        ViewBag.User = _context
+            .Users
+            .Find(HttpContext.Session.GetInt32("UserId"));
 
         return View(); //this view goes to MoivePage.cshtml
     }
@@ -112,7 +133,7 @@ public class HomeController : Controller
     // User in session  unlike to the movie
     [HttpPost("movies/{id}/likes/delete")]
     public IActionResult RemoveLikeFromMovie(int id)
-    {   
+    {
         //get the userId
         var userId = HttpContext.Session.GetInt32("UserId").GetValueOrDefault();
         //get the like that need to remove
@@ -120,8 +141,24 @@ public class HomeController : Controller
             .Likes
         //get the first like where the movie id matches the id we took in the form, and the user id in session matches the user id
             .FirstOrDefault(like => like.MovieId == id && like.UserId == userId);
-            
+
         _context.Likes.Remove(likeToRemove);
+        _context.SaveChanges();
+
+        return RedirectToAction("Dashboard");
+    }
+
+    // User who posted can delte
+    [HttpPost("movies/{id}/delete")]
+    public IActionResult DeleteMovie(int id)
+    {
+        //get the movie that need to remove
+        var movieToDelete = _context
+            .Movies
+        //get the first like where the movie id matches the id we took in the form, and the user id in session matches the user id
+            .Find(id);
+
+        _context.Movies.Remove(movieToDelete);
         _context.SaveChanges();
 
         return RedirectToAction("Dashboard");
